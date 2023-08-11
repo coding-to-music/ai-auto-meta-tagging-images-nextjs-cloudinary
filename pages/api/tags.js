472c882a29fac9f5bb483bcc6c15e1c315ed5b0b
coding-to-music <connors.tom@gmail.com>
2023-08-11 16:13:28 -0500

    Build

--- pages/api/tags.js
+++ pages/api/tags.js
@@ -6,6 +6,11 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// console.log("cloud_name", process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
+// console.log("api_key", process.env.CLOUDINARY_API_KEY);
+// console.log("api_secret", process.env.CLOUDINARY_API_SECRET);
+// // console.log("folder_name", folder_name);
+
 export default async function handler(req, res) {
   const tags = await cloudinary.api.tags({
     max_results: 50,
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   const [imageSrc, setImageSrc] = useState();
@@ -10,9 +10,9 @@ export default function Home() {
 
   useEffect(() => {
     (async function run() {
-      const data = await fetch('/api/tags').then(r => r.json());
+      const data = await fetch("/api/tags").then((r) => r.json());
       setTags(data.tags);
-    })()
+    })();
   }, []);
 
   const [activeTag, setActiveTag] = useState();
@@ -20,15 +20,15 @@ export default function Home() {
 
   useEffect(() => {
     (async function run() {
-      if ( !activeTag ) return;
-      const data = await fetch('/api/images', {
-        method: 'POST',
+      if (!activeTag) return;
+      const data = await fetch("/api/images", {
+        method: "POST",
         body: JSON.stringify({
-          tag: activeTag
-        })
-      }).then(r => r.json());
+          tag: activeTag,
+        }),
+      }).then((r) => r.json());
       setImages(data.resources);
-    })()
+    })();
   }, [activeTag]);
 
   /**
@@ -39,10 +39,10 @@ export default function Home() {
   function handleOnChange(changeEvent) {
     const reader = new FileReader();
 
-    reader.onload = function(onLoadEvent) {
+    reader.onload = function (onLoadEvent) {
       setImageSrc(onLoadEvent.target.result);
       setUploadData(undefined);
-    }
+    };
 
     reader.readAsDataURL(changeEvent.target.files[0]);
   }
@@ -55,12 +55,12 @@ export default function Home() {
   async function handleOnSubmit(event) {
     event.preventDefault();
 
-    const data = await fetch('/api/upload', {
-      method: 'POST',
+    const data = await fetch("/api/upload", {
+      method: "POST",
       body: JSON.stringify({
-        image: imageSrc
-      })
-    }).then(r => r.json());
+        image: imageSrc,
+      }),
+    }).then((r) => r.json());
 
     setUploadData(data);
   }
@@ -69,64 +69,80 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Image Uploader</title>
-        <meta name="description" content="Upload your image to Cloudinary!" />
+        <meta
+          name="description"
+          content="Upload with auto-meta-tags your image to Cloudinary!"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Image Uploader
-        </h1>
-
+        <h1 className={styles.title}>Image Uploader</h1>
 
         {Array.isArray(tags) && (
-          <ul style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            listStyle: 'none',
-            padding: 0,
-            margin: 0
-          }}>
-            { tags.map(tag => {
+          <ul
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              listStyle: "none",
+              padding: 0,
+              margin: 0,
+            }}
+          >
+            {tags.map((tag) => {
               return (
-                <li key={tag} style={{ margin: '.5em' }}>
-                  <button onClick={() => setActiveTag(tag)} style={{
-                    color: 'white',
-                    backgroundColor: tag === activeTag ? 'blue' : 'blueviolet'
-                  }}>
-                    { tag }
+                <li key={tag} style={{ margin: ".5em" }}>
+                  <button
+                    onClick={() => setActiveTag(tag)}
+                    style={{
+                      color: "white",
+                      backgroundColor:
+                        tag === activeTag ? "blue" : "blueviolet",
+                    }}
+                  >
+                    {tag}
                   </button>
                 </li>
-              )
+              );
             })}
           </ul>
         )}
 
         {Array.isArray(images) && (
-          <ul style={{
-            display: 'grid',
-            gridGap: '1em',
-            gridTemplateColumns: 'repeat(auto-fit, minmax(150px, 1fr))',
-            listStyle: 'none',
-            padding: 0,
-            margin: 0
-          }}>
-            { images.map(image => {
+          <ul
+            style={{
+              display: "grid",
+              gridGap: "1em",
+              gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
+              listStyle: "none",
+              padding: 0,
+              margin: 0,
+            }}
+          >
+            {images.map((image) => {
               return (
-                <li key={image.asset_id} style={{ margin: '1em' }}>
-                  <img src={image.secure_url} width={image.width} height={image.height} alt="" />
+                <li key={image.asset_id} style={{ margin: "1em" }}>
+                  <img
+                    src={image.secure_url}
+                    width={image.width}
+                    height={image.height}
+                    alt=""
+                  />
                 </li>
-              )
+              );
             })}
           </ul>
         )}
 
-        <p className={styles.description}>
-          Upload your image to Cloudinary!
-        </p>
+        <p className={styles.description}>Upload your image to Cloudinary!</p>
 
-        <form className={styles.form} method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+        <form
+          className={styles.form}
+          method="post"
+          onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
+        >
           <p>
             <input type="file" name="file" />
           </p>
@@ -140,14 +156,19 @@ export default function Home() {
           )}
 
           {uploadData && (
-            <code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
+            <code>
+              <pre>{JSON.stringify(uploadData, null, 2)}</pre>
+            </code>
           )}
         </form>
       </main>
 
       <footer className={styles.footer}>
-        <p>Find the tutorial on <a href="https://spacejelly.dev/">spacejelly.dev</a>!</p>
+        <p>
+          Find the tutorial on{" "}
+          <a href="https://spacejelly.dev/">spacejelly.dev</a>!
+        </p>
       </footer>
     </div>
-  )
+  );
 }
