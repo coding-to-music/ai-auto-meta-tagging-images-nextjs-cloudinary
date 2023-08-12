314edae3db68261d56838461ea1353c9dda31d1e
coding-to-music <connors.tom@gmail.com>
2023-08-11 22:32:19 -0500

    Build

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -8,12 +8,10 @@ cloudinary.config({
 
 const folder_name = process.env.CLOUDINARY_FOLDER_NAME;
 
-// const folder_name = process.env.CLOUDINARY_FOLDER_NAME;
-
 // console.log('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
 // console.log('api_key', process.env.CLOUDINARY_API_KEY);
 // console.log('api_secret', process.env.CLOUDINARY_API_SECRET);
-console.log("folder_name", folder_name);
+// console.log("folder_name", folder_name);
 
 export default async function handler(req, res) {
   const { image } = JSON.parse(req.body);
