533e824e6342b9657252995810c77e5127b79292
coding-to-music <connors.tom@gmail.com>
2023-08-11 21:41:53 -0500

    Build

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -4,19 +4,21 @@ cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  folder_name: process.env.CLOUDINARY_FOLDER_NAME,
 });
+
+const folder_name = process.env.CLOUDINARY_FOLDER_NAME;
 
 // const folder_name = process.env.CLOUDINARY_FOLDER_NAME;
 
 // console.log('cloud_name', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
 // console.log('api_key', process.env.CLOUDINARY_API_KEY);
 // console.log('api_secret', process.env.CLOUDINARY_API_SECRET);
-// console.log('folder_name', folder_name);
+console.log("folder_name", folder_name);
 
 export default async function handler(req, res) {
   const { image } = JSON.parse(req.body);
   const results = await cloudinary.uploader.upload(image, {
+    folder: `${folder_name}`,
     categorization: "google_tagging",
     auto_tagging: 0.6,
   });
